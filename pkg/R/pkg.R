29d8e557f4d02e73d869616b6b1b53fa3faa0d5e
Martijn Tennekes <mtennekes@gmail.com>
2015-05-08 16:53:28 +0200

    first commit

---
+++ pkg/R/pkg.R
@@ -0,0 +1,19 @@
+#' Spatial Toolbox
+#'
+#' \tabular{ll}{
+#' Package: \tab tmap\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.8\cr
+#' Date: \tab 2015-04-24\cr
+#' License: \tab GPL-3\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Miscellaneous spatial tools, such as creating smooth polylines from points.
+#' 
+#' @name spatialTools-package
+#' @aliases spatialTools
+#' @docType package
+#' @author Martijn Tennekes \email{mtennekes@@gmail.com}
+#' @keywords GIS
+NULL
